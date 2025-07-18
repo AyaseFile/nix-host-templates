16dd7b9efff6205781fe049c8592c9d27d65c2a8
Ayase <166034664+AyaseFile@users.noreply.github.com>
2025-07-18 20:09:05 +0800

    nixos & lxc: openssh security settings

--- nixos/btrfs-swap/config.nix
+++ nixos/btrfs-swap/config.nix
@@ -95,6 +95,20 @@
     settings = {
       PermitRootLogin = "no";
       PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      Ciphers = [
+        "chacha20-poly1305@openssh.com"
+        "aes256-gcm@openssh.com"
+      ];
+      KexAlgorithms = [
+        "mlkem768x25519-sha256"
+        "sntrup761x25519-sha512"
+        "sntrup761x25519-sha512@openssh.com"
+      ];
+      Macs = [
+        "hmac-sha2-512-etm@openssh.com"
+        "hmac-sha2-256-etm@openssh.com"
+      ];
     };
   };
 
